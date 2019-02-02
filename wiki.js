b72b0c1d8fb2931028c244056a7f7adc6e96ec9c
Muhammad <myhoomac@hotmail.com>
2019-02-02 09:27:51 +0700

    hurry up

--- wiki.js
+++ wiki.js
@@ -1,4 +1,3 @@
-const wikipedia = ``; // API
+const wikipedia = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchThem}`; // API
 const find = el => document.querySelector(el);
 const create = el => document.createElement(el);
-console.log(create);
