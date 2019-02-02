b46245319f1f5a2b7d7346d80c5179e7de1f056b
Muhammad <myhoomac@hotmail.com>
2019-02-02 19:16:37 +0700

    hurry up

--- wiki.js
+++ wiki.js
@@ -1,19 +1,20 @@
 // functions for DOM,
 const [find, create, textNode] = [el => document.querySelector(el), el => document.createElement(el), el => document.createTextNode(el)];
 
-const lookingThem = find('.searchButton').addEventListener('click', events => {
+const finder = find('.searchButton').addEventListener('click', events => {
   events.preventDefault()
-  const inputText = find('.box').value.trim();
+  const inputText = find('.box').value.trim(); // space includes string and has length, trim them
   // console.log(inputText.length);
   result(inputText);
 })
 
 const result = searching => {
-  const wikipedia = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searching}`; // API
+  const wikipedia = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=30&srsearch=${searching}`; // API
   fetch(wikipedia)
     .then(resolve => resolve.json())
     .then(data => {
       const somewords = find('.box').value;
+      console.log(data);
       const dataFile = data.query.search;
       //check input data if can't found
       typeof data.continue === 'undefined' ? displayCantFound(somewords) : displayData(dataFile);
@@ -25,7 +26,7 @@ const displayCantFound = theInputValue => {
   find('.result').innerHTML =
     `<ul class='flex-content wrapIt'>
       <li class='item'> <h1> Ooopss can't found: ${theInputValue} </h1></li>
-      <li class='item'> <img class='pic' src='clown-sad.png'> </> </li>
+      <li class='item'> <img class='pic' src='clown-sad.png'> </li>
    </ul>`;
 }
 
@@ -34,7 +35,15 @@ const displayData = dataQuery => {
   putTextData.innerHTML = '';
   // console.log(dataQuery);
   for (data of dataQuery) {
-    const url = encodeURI(`https://en.wikipedia.org/wiki/${data.title}`);
     console.log(data);
+    const url = encodeURI(`https://en.wikipedia.org/wiki/${data.title}`);
+    putTextData.insertAdjacentHTML('beforeend',
+    `<div class='datatext'>
+      <h3 class='textTittle'>
+        <a href='${url}' target=_blank>${data.title}</a>
+      </h3>
+      <p class='paragraf'>  ${data.snippet} </p>
+      <a href='${url}' target=_blank> <span> ${url} </span></a>
+    </div>`)
   }
 }
