ee6b515836af862ce7ab213f43e967313ecd794d
Muhammad <myhoomac@hotmail.com>
2019-02-02 16:36:57 +0700

    hurry up

--- wiki.js
+++ wiki.js
@@ -1,9 +1,9 @@
 const find = el => document.querySelector(el)
 const create = el => document.createElement(el)
 
-const looking = find('.searchButton').addEventListener('click', (e) => {
+const lookingThem = find('.searchButton').addEventListener('click', (e) => {
   e.preventDefault()
-  const inputText = find('.box').value.trim(); // space termasuk length, harus di trim
+  const inputText = find('.box').value.trim();
   // console.log(inputText.length);
   result(inputText);
 })
@@ -11,6 +11,24 @@ const looking = find('.searchButton').addEventListener('click', (e) => {
 const result = searching => {
   const wikipedia = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searching}`; // API
   fetch(wikipedia)
-    .then(resolve => console.log(resolve.json()))
-    .then(data => console.log(data))
+    .then(resolve => resolve.json())
+    .then(data => {
+      if(typeof data.continue === 'undefined'){
+        displayCantFound(find('.box').value)
+      } else {
+        console.log(data);
+      }
+    })
+    .catch((e) => alert('an error occurred', e))
 }
+
+const displayCantFound = theInputValue => {
+  find('.result').innerHTML =
+  `<div>
+      <h3> sorry can't found what you are looking ${theInputValue} </h3>
+  </div>`
+}
+
+// const displayError = () => {
+//
+// }
